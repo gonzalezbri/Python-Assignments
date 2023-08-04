716051d74bb33bde9a86e1d98b7addfa5ab94015
gonzalezbri <4717bg@gmail.com>
2023-08-03 18:45:30 -0700

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,21 @@
+def hello():
+    print("Welcome to our website")
+
+def pack(bags, luggage, backpacks):
+    return [bags, luggage, backpacks]
+
+my_list = ["fish", "chips", "bread", "apple"]
+
+def eat_lunch(my_list):
+    if not my_list:
+        print("My lunchbox is empty")
+    else:
+        for i in range(len(my_list)):
+            if i == 0:
+                print(f"First I eat {my_list[i]}")
+            else:
+                print(f"Next I eat {my_list[i]}")
+
+hello()
+print(pack("bags", "suitcase", "backpack"))
+eat_lunch(my_list)
